35e9fcc8f9727480053b7a954215ebe928ff039f
Aron Granberg <aron.granberg@gmail.com>
2022-09-19 10:42:46 +0200

    Extend 'message' demo.

--- examples/message.rs
+++ examples/message.rs
@@ -2,8 +2,17 @@ use advance::ProgressBar;
 use std::{thread::sleep, time::Duration};
 
 pub fn main() {
-    let p = ProgressBar::new().with_message("Calibrating flux capacitors");
-    for _ in p.wrap(0..100) {
-        sleep(Duration::from_millis(20));
-    }
+    let mut p = ProgressBar::new().split_sized();
+    let mut flux = p.take(1).with_message("Calibrating flux capacitors");
+    let mut heavy = p.take(1).with_message("Heavy work!");
+    let mut important = p.take(1).with_message("More important work");
+
+    sleep(Duration::from_millis(1000));
+    flux.finish();
+
+    sleep(Duration::from_millis(1000));
+    heavy.finish();
+
+    sleep(Duration::from_millis(1000));
+    important.finish();
 }
